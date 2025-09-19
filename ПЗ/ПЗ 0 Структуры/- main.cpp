0c911d98269e514cdc03a6cd208b84cb6072743b
MiPi <mishutik.p@gmail.com>
2025-09-19 12:43:44 +0300

    Пересечение фигур
    Добавил:
    1)Создать функцию, проверяющую пересечение двух кругов.
    2)Создать функцию, проверяющую пересечение двух квадратов.

--- ПЗ/ПЗ 0 Структуры/- main.cpp
+++ ПЗ/ПЗ 0 Структуры/- main.cpp
@@ -106,6 +106,36 @@ bool border_point_in_square(const Point &p, const Square &s){
 
 // Пересечение фигур ...
 
+bool intersection_of_circles(const Circle &c, const Circle &c2){
+    double dx = c.center.x - c2 .center.x;
+    double dy = c.center.y - c2.center.y;
+    double d = sqrt(dx*dx + dy*dy);
+    return (fabs(c.r - c2.r) <= d) and (d <= c.r + c2.r);
+}
+
+bool intersection_of_square(const Square &s, const Square &s2){
+    double s_left = s.top.x;
+    double s_right = s.top.x + s.side;
+    double s_top = s.top.y;
+    double s_bottom = s.top.y - s.side;
+
+    double s2_left = s2.top.x;
+    double s2_right = s2.top.x + s2.side;
+    double s2_top = s2.top.y;
+    double s2_bottom = s2.top.y - s2.side;
+
+    if (s_left > s2_right or s_right < s2_left or
+        s_top < s2_bottom or s_bottom > s2_top){
+            return 0; // не пересекаются
+        }
+    return 1; // пересекаются 
+}
+
+
+
+
+
+
 
 
 
@@ -139,38 +169,63 @@ int main(){
 
     cout << "Принадлежность точки фигуре (без границ):" << endl;
 
-    cout << "Точка в круг: ";
+    cout << "Точка в кругу? ";
     if (point_in_circle(a, b) == true){
-        cout << "Принадлежит"  << endl;
+        cout << "Да"  << endl;
     } else {
-        cout << "Не принадлежит"  << endl;
+        cout << "Нет"  << endl;
     }
 
-    cout << "Точка в квадрат: ";
+    cout << "Точка в квадрате: ";
     if (point_in_square(a, c) == true){
-        cout << "Принадлежит"  << endl;
+        cout << "Да"  << endl;
     } else {
-        cout << "Не принадлежит"  << endl;
+        cout << "Нет"  << endl;
     }
 
 
 
     cout << "Нахождение точки на контуре (граница)" << endl;
     
-    cout << "Точка круг граница: ";
+    cout << "Точка на границе круга? ";
     if (border_point_in_circle(a, b) == true){
-        cout << "На границе груга" << endl;
+        cout << "Да" << endl;
     } else {
-        cout << "Не на границе круга" << endl;
+        cout << "Нет" << endl;
     }
 
-    cout << "Точка квадрат граница: ";
+    cout << "Точка на границе квадрата? ";
     if (border_point_in_square(a, c) == true){
-        cout << "На границе квадрата" << endl;
+        cout << "Да" << endl;
     } else {
-        cout << "Не на границе квадрата" << endl;
+        cout << "Нет" << endl;
     }
-    
+
+
+
+    cout << "Пересечение фигур" << endl;
+
+    Circle b2; // доп круг
+    cout << "Введите доп круг" << endl;
+    readCircle(b2);
+    cout << "Круги пересекаются? ";
+    if (intersection_of_circles(b, b2) == true){
+        cout << "Да" << endl;
+    } else {
+        cout << "Нет" << endl;
+    }
+
+    Square c2; //доп квадрат
+    cout << "Введите доп квадрат" << endl;
+    readSquare(c2);
+    cout << "Квадраты пересекаются? ";
+    if (intersection_of_square(c, c2) == true){
+        cout << "Да" << endl;
+    } else {
+        cout << "Нет" << endl;
+    }
+
+
 
 
 }
