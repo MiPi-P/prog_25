6ca4ea79f1b6770c907950dca0c81ab1bce987ae
MiPi <mishutik.p@gmail.com>
2025-10-06 11:05:36 +0300

    Задание 1

---
+++ PZ/PZ_1_class/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class MyArray {
+private:
+    int* data;  // указатель на массив
+    int size;   // количество элементов
+
+public:
+
+    MyArray(int n){ // конструктор
+        size = n;
+        data = new int[size];
+
+        for (int i = 0; i != size; i += 1){ // заполняем 0
+            data[i] = 0;
+        }
+    }
+
+    ~MyArray() { // деконструктор, 
+    delete[] data;
+    }
+
+
+    void setValue(int index, int value){ // сеттер
+        if (0 <= index and index < size){
+            if (-100 <= value and value <= 100){
+                data[index] = value;
+            } else {
+                cout << "Ошибка в значении" << endl;
+                return;
+            }
+        } else {
+            cout << "Ошибка в индексе" << endl;
+            return;
+        }
+    }
+
+    int getValue(int index){
+        if (0 <= index and index < size){
+            return data[index];
+        } else {
+            cout << "Ошибка индекса" << endl;
+            return 0;
+        }
+    }
+
+    void print(){
+        for (int i = 0; i != size; i += 1){
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+
+    
+};
+
+int main(){
+    MyArray a(5);
+    a.print();
+    a.setValue(1, 5);
+    a.print();
+    cout << a.getValue(1);
+}
