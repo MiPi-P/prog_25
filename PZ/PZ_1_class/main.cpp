746ea9b765dce1a75b774bea6f51d24d75116adf
MiPi <mishutik.p@gmail.com>
2025-10-06 15:16:08 +0300

    №4
    классы готовы

--- PZ/PZ_1_class/main.cpp
+++ PZ/PZ_1_class/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class MyArray {
 private:
     int* data;  // указатель на массив
-    int size;   // количество элементов
+    int size;   // кол-во элементов
 
 public:
 
@@ -22,7 +22,8 @@ public:
         }
     }
 
-    MyArray(const MyArray& other){ // rjycnhernjh2
+    //#2
+    MyArray(const MyArray& other){ // конструктор 2
         size = other.size;
         data = new int[size];
         for (int i = 0; i < size; i += 1) {
@@ -34,6 +35,7 @@ public:
     ~MyArray() { // деконструктор, 
     delete[] data;
     }
+
 
 
     void setValue(int index, int value){ // сеттер
@@ -67,7 +69,7 @@ public:
     }
 
 
-
+    //#3
     void addValue(int value){
         if (-100 <= value and value <= 100){
             int* newData = new int[size + 1];
@@ -88,15 +90,57 @@ public:
         }
     }
 
+
+
+    // номер 4
+    void add(const MyArray& other){
+        int minSize = min(size, other.size);
+
+        for (int i = 0; i < minSize; i += 1){
+            data[i] += other.data[i];
+        }
+    }
+
+    void sub(const MyArray& other){
+        int minSize = min(size, other.size);
+
+        for (int i = 0; i < minSize; i += 1){
+            data[i] -= other.data[i];
+        }
+    }
+
+
+
     
 };
 
 int main(){
-    MyArray a(5);
+    MyArray a(3);
     a.print();
-    a.setValue(1, 5);
+    a.setValue(0, 1);
+    a.setValue(1, 2);
+    a.setValue(2, 3);
     a.print();
     cout << a.getValue(1) << endl;
-    a.addValue(99);
+    a.addValue(4);
     a.print();
+
+
+    cout << "\n#4\n";
+    MyArray b(3);
+    b.setValue(0, 10);
+    b.setValue(1, 20);
+    b.setValue(2, 30);
+    cout << "a = ";
+    a.print();
+    cout << "b = " ;
+    b.print();
+    cout << "a + b: ";
+    a.add(b);
+    a.print();
+    cout << "a(новый) - b: ";
+    a.sub(b);
+    a.print();
+
+
 }
