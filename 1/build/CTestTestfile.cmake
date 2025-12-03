2eefa18c3989c270cc32f1a34aaea3aff1244a6e
MiPi <mishutik.p@gmail.com>
2025-12-03 14:36:35 +0300

    пз6

--- 1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mi/Documents/c++/prog_25/1
-# Build directory: C:/Users/Mi/Documents/c++/prog_25/1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
--- HMI2_1/include/HMI2/Widget.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-class Widget {
-public:
-    virtual void draw(sf::RenderWindow& window) = 0;
-    virtual void update() {}   // для будущего обновления по переменной
-    virtual ~Widget() = default;
-};
